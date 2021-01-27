565daf4b3871a120bc492b2256629580c3e72ce1
Yilikaer Yihamujiang <yilikaer.yihamujiang@tuni.fi>
2021-01-27 13:42:20 +0200

    harjoitus 4.4.2

--- GIT_EX/temperature.py
+++ GIT_EX/temperature.py
@@ -18,10 +18,10 @@ def main():
 def t_transform(temp, type):
 
     if type == 'c':
-        return float(temp * 9/15 + 32)
+        return float(temp * 9/5 + 32)
 
     elif type == 'f':
-        return float((temp - 32) / (9 / 15))
+        return float((temp - 32) / (9/5))
 
     return None
 
